fb360b2d6e869f578abd1681b3ed4737887f3b9a
Steffano Martinez <steffano1213@gmail.com>
2023-04-19 18:24:09 -0400

    Navbar and footer issues #1 and #3 ongoing

---
+++ frontend/src/components/navbar/Navbar.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import NavbarBody from './NavbarBody';
+import MyRoute from "./Route";
+
+
+
+export default function MyNavbar() {
+    return (
+        <>
+        <NavbarBody />
+        <MyRoute />
+        </>
+    )
+}
