ecd6e5652c9e20354418b59640039f7535c4f005
Steffano Martinez <steffano1213@gmail.com>
2023-04-20 00:16:47 -0400

    Added some changes to navbar, still need login

--- frontend/src/components/navbar/NavbarBody.js
+++ frontend/src/components/navbar/NavbarBody.js
@@ -1,13 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import logo from '../../reelgood_logo.png';
-import { Navbar, Nav, Container, Image} from 'react-bootstrap'
+import { Navbar, Nav, Container, Image } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import MySearchBar from './SearchBar';
 export default function MyNavbar() {
     return (
         <Navbar bg="dark" variant="dark" expand="lg">
-            <Container className="mb-6" fluid> {/* Warning: bottom marging might exceed visual.*/}
-                    <Image src={logo} alt="Logo" width={80} height={60} /> 
+            <Container fluid> {/* Warning: bottom marging might exceed visual.*/}
+                <Image src={logo} alt="Logo" width={80} height={60} />
                 <Navbar.Toggle aria-controls="basic-navbar=nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav>
@@ -18,9 +18,7 @@ export default function MyNavbar() {
                         <Nav.Link as={Link} to='/upcoming'>Upcoming</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
-            </Container>
-            <Container> {/* Move TO THE RIGTH*/}
-                <MySearchBar />
+                <MySearchBar/>
             </Container>
         </Navbar>
     )
