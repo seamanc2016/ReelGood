5efe84fe39728a40c9492e7e2e420d9666486a63
Steffano Martinez <steffano1213@gmail.com>
2023-04-21 22:22:28 -0400

    Added Footer with logos

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,19 @@
 import './App.css';
 import { AuthContextProvider } from './Context/authContext';
 import MyNavbar from './components/navbar/Navbar'
-
+import MyFooter from './components/footer/Footer'
 import { AuthContext } from './Context/authContext'
 
 function App() {
   return (
-    <>
-    <AuthContextProvider> {/*Creates Scope so that child components can use Auth and setAuth */}
-      <MyNavbar />
-    </AuthContextProvider>
-    </>
+    <div className="page-container">
+      <div className="content-wrap">
+        <AuthContextProvider> {/*Creates Scope so that child components can use Auth and setAuth */}
+          <MyNavbar />
+        </AuthContextProvider>
+      </div>
+      <MyFooter />
+    </div>
   );
 }
 
