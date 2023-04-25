8019b6df6247f366de24a200e710ddbe0a133ac1
Joshua Henry <81492473+jhenry02@users.noreply.github.com>
2023-04-25 00:32:30 -0400

    Merge pull request #61 from cop4808-spring-2023-fullstack-web/steffano-dev
    I dont even know, but it's theater search related

--- frontend/src/pages/Theater.js
+++ frontend/src/pages/Theater.js
@@ -1,14 +1,30 @@
-import { Container } from "react-bootstrap";
-import MyTheaterCard from "../../src/components/theatercard/TheaterCard";
-import MyTheaterSearchBar from "../../src/components/theatersearchbar/TheaterSearchBar";
-import PageNavigation from "../../src/components/pagenavigation/PageNavigation"
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { Container } from 'react-bootstrap';
+import MyTheaterCard from '../../src/components/theatercard/TheaterCard';
+import MyTheaterSearchBar from '../../src/components/theatersearchbar/TheaterSearchBar';
+import PageNavigation from '../../src/components/pagenavigation/PageNavigation';
+
 export default function Theater() {
+  const [theaters, setTheaters] = useState([]);
+
+  function handleSearch(location, sortBy, page) {
+    axios
+      .get(`http://localhost:5678/theaters?location=${location}&sort_by=${sortBy}&page=${page}`)
+      .then((response) => {
+        setTheaters(response.data.businesses);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   return (
     <>
       <Container className="my-5 ">
-        <MyTheaterSearchBar />
+        <MyTheaterSearchBar onSearch={handleSearch} />
       </Container>
-      <MyTheaterCard />
+      <MyTheaterCard theaters={theaters} />
     </>
-  )
+  );
 }
