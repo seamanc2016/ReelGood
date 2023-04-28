53a626138a175b6c83e08362db0b4ee99b55f931
seamanc2016 <seamanc2016@fau.edu>
2023-04-28 00:25:46 -0400

    Added new MovieDetails page

---
+++ frontend/src/pages/MovieDetails.js
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import MovieDetailsCard from './components/moviedetailscard/MovieDetailsCard';
+import ActorList from './components/actorlist/ActorList';
+import RecommendationList from './components/recommendationlist/RecommendationList';
+
+function MovieDetails() {
+  //Right now, I'm emulating the MovieDetailsPage. Technically, it will get a prop to set movieID initial state width.
+  let [movieID, setMovieID] = useState(315162);
+  let [history, setHistory] = useState([]);
+
+  //Go back to search results page
+  //Not added yet
+
+  //Go back to previous movie viewed
+  function goToPreviousMovie(history) {
+    console.log(history);
+    setHistory(history.slice(0, -1));
+    setMovieID(history[history.length - 1]);
+  }
+
+  return (
+    <>
+      {/*Navigation Buttons*/}
+      <div className="text-center mt-3">
+        <button className="btn btn-dark me-1">Go Back To Search Results</button>
+        {history && history.length > 0 && (<button className="btn btn-dark me-1" onClick={() => goToPreviousMovie(history)}>View Previous</button>)}
+      </div>
+      
+      {/*Main content*/}
+      <MovieDetailsCard movieID={movieID} />
+      <ActorList movieID={movieID} />
+      <RecommendationList setMovieID={setMovieID} setHistory={setHistory} movieID={movieID} history={history} />
+
+    </>
+  );
+}
+
+export default MovieDetails;
