b3918b9c28796290ec969c238dd4a2b81bf7899b
seamanc2016 <seamanc2016@fau.edu>
2023-04-28 17:59:24 -0400

    Implemented movie searching capabilities on Navbar

--- frontend/src/components/navbar/Route.js
+++ frontend/src/components/navbar/Route.js
@@ -10,43 +10,46 @@ import Login from '../../pages/Login';
 import Register from '../userauth/Register';
 import Error from '../Error/Error';
 import MovieDetails from '../../pages/MovieDetails';
+import MovieSearch from '../../pages/MovieSearch';
 
-import {useContext} from 'react';
+import { useContext } from 'react';
 import { UserContext } from '../../Context/UserContext';
 
 export default function MyRouter() {
 
-    const {User, setUser} = useContext(UserContext);
+    const { User, setUser } = useContext(UserContext);
 
-    if(User){
-    return (
-        <>
-            <Routes> 
-                <Route exact path="/" element={<Home />}></Route>
-                <Route exact path='/Nowplaying' element={<NowPlaying />}></Route>
-                <Route exact path='/Popular' element={<Popular />}></Route>
-                <Route exact path='/Theater' element={<Theater />}></Route>
-                <Route exact path='/Upcoming' element={<Upcoming />}></Route>
-                <Route exact path='/Login' element={<Login />}></Route>
-                <Route exact path='/Register' element={<Register />}></Route>
-                <Route path="/movies/:id" element={<MovieDetails />} />
-            </Routes>
-        </>);
-    } else if(!User)
-    {   return(
-        <>
-            <Routes> 
-                <Route exact path="/" element={<Home />}></Route>
-                <Route exact path='/Nowplaying' element={<Error />}></Route>
-                <Route exact path='/Popular' element={<Error />}></Route>
-                <Route exact path='/Theater' element={<Error />}></Route>
-                <Route exact path='/Upcoming' element={<Error />}></Route>
-                <Route exact path='/Login' element={<Login />}></Route>
-                <Route exact path='/Register' element={<Register />}></Route>
-                <Route path="/movies/:id" element={<Error />} />
-            </Routes>
-        </>
+    if (User) {
+        return (
+            <>
+                <Routes>
+                    <Route exact path="/" element={<Home />}></Route>
+                    <Route exact path='/Nowplaying' element={<NowPlaying />}></Route>
+                    <Route exact path='/Popular' element={<Popular />}></Route>
+                    <Route exact path='/Theater' element={<Theater />}></Route>
+                    <Route exact path='/Upcoming' element={<Upcoming />}></Route>
+                    <Route exact path='/Login' element={<Login />}></Route>
+                    <Route exact path='/Register' element={<Register />}></Route>
+                    <Route path="/movies/:id" element={<MovieDetails />} />
+                    <Route path="/search/:query" element={<MovieSearch />}></Route>
+                </Routes>
+            </>);
+    } else if (!User) {
+        return (
+            <>
+                <Routes>
+                    <Route exact path="/" element={<Home />}></Route>
+                    <Route exact path='/Nowplaying' element={<Error />}></Route>
+                    <Route exact path='/Popular' element={<Error />}></Route>
+                    <Route exact path='/Theater' element={<Error />}></Route>
+                    <Route exact path='/Upcoming' element={<Error />}></Route>
+                    <Route exact path='/Login' element={<Login />}></Route>
+                    <Route exact path='/Register' element={<Register />}></Route>
+                    <Route path="/movies/:id" element={<Error />} />
+                    <Route path="/search/:query" element={<Error />}></Route>
+                </Routes>
+            </>
         );
     }
-     
+
 }
