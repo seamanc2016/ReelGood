f6a0191c8974e91f7329752e43b11a1f146ebfa4
seamanc2016 <seamanc2016@fau.edu>
2023-04-28 19:27:56 -0400

    Accounting for authstate when rendering Navbar features

--- frontend/src/components/navbar/NavbarBody.js
+++ frontend/src/components/navbar/NavbarBody.js
@@ -3,12 +3,12 @@ import logo from '../../images/reelgood_logo.png';
 import { Navbar, Nav, Form, Button, ButtonGroup, Image, Container } from 'react-bootstrap'
 import MySearchBar from './SearchBar';
 
-import {Link, useNavigate} from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 import { useEffect, useContext } from 'react';
 import { UserContext } from '../../Context/UserContext';
 
-import {getAuth, signOut} from 'firebase/auth';
+import { getAuth, signOut } from 'firebase/auth';
 import '../../config/firebase-config';
 
 
@@ -18,33 +18,33 @@ export default function MyNavbar() {
     const navigate = useNavigate();
 
     // Using UserContext to access Use and setUser
-    const {User, setUser} = useContext(UserContext);
+    const { User, setUser } = useContext(UserContext);
 
     const signout = (e) => {
         e.preventDefault();
-    
+
         // authenticates with google using api credentals
         const auth = getAuth()
         auth.signOut()
-        .then(() => {
-            navigate('/');  // navigate back to home page
-            setUser(null);
-            localStorage.clear()
-        }).catch((e) => { // else catch and print errors
-          console.log(e.code)
-          console.log(e.message);
-        });
+            .then(() => {
+                navigate('/');  // navigate back to home page
+                setUser(null);
+                localStorage.clear()
+            }).catch((e) => { // else catch and print errors
+                console.log(e.code)
+                console.log(e.message);
+            });
     }
 
     const authbutton = () => {
-        if(!User){  // If Not Authenticated Show Login and Signup
-            return(
-            <ButtonGroup>
-                <Button variant="outline-light" as={Link} to="/Login">Login</Button>
-                <Button variant="outline-light" as={Link} to="/Register">Signup</Button>
-            </ButtonGroup>
+        if (!User) {  // If Not Authenticated Show Login and Signup
+            return (
+                <ButtonGroup>
+                    <Button variant="outline-light" as={Link} to="/Login">Login</Button>
+                    <Button variant="outline-light" as={Link} to="/Register">Signup</Button>
+                </ButtonGroup>
             );
-        }else{  // If Authenticated Show Logout
+        } else {  // If Authenticated Show Logout
             return (<Button variant="warning" onClick={signout}>Logout</Button>);
         }
 
@@ -55,19 +55,28 @@ export default function MyNavbar() {
             <Container fluid> {/* Warning: bottom marging might exceed visual.*/}
                 <Image src={logo} alt="Logo" width={80} height={60} />
                 <Navbar.Toggle aria-controls="basic-navbar=nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">            
-                        <Nav>
-                            <Nav.Link as={Link} to='/'>Home</Nav.Link>
-                            <Nav.Link as={Link} to='/nowplaying'>Now Playing</Nav.Link>
-                            <Nav.Link as={Link} to='/popular'>Popular</Nav.Link>
-                            <Nav.Link as={Link} to='/upcoming'>Upcoming</Nav.Link>
-                            <Nav.Link as={Link} to='/theater'>Theater</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                <MySearchBar/> {/* This component loads the searchbar component. */}
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav>
+                        {User && (
+                            <>
+                                <Nav.Link as={Link} to='/'>Home</Nav.Link>
+                                <Nav.Link as={Link} to='/nowplaying'>Now Playing</Nav.Link>
+                                <Nav.Link as={Link} to='/popular'>Popular</Nav.Link>
+                                <Nav.Link as={Link} to='/upcoming'>Upcoming</Nav.Link>
+                                <Nav.Link as={Link} to='/theater'>Theater</Nav.Link>
+                            </>
+                        )}
+                    </Nav>
+                </Navbar.Collapse>
+                {User && (
+                    <>
+                        <MySearchBar /> {/* This component loads the searchbar component. */}
+                    </>
+                )}
+
                 <Form inline className="mx-3">
-                        {authbutton()}
-                    </Form>
+                    {authbutton()}
+                </Form>
             </Container>
         </Navbar>
     )
