8b81809f09e5cd6a790ac4b821bb1c42740728e8
Steffano Martinez <steffano1213@gmail.com>
2023-04-29 19:20:25 -0400

    Added a dropdown to access user profile or logout

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,9 @@
 import MyCarousel from '../components/carousel/Carousel'
+
 export default function Home() {
     return (
         <>
-        <MyCarousel /> {/* This component just loads the content of the carousel. */}
+            <MyCarousel />
         </>
     )
 }
