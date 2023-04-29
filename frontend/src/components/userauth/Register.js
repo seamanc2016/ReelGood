1852df6a012f587b6c71e4a680f552203b4ac1ce
Steffano Martinez <steffano1213@gmail.com>
2023-04-29 00:17:46 -0400

    Added front end only for registration

--- frontend/src/components/userauth/Register.js
+++ frontend/src/components/userauth/Register.js
@@ -2,25 +2,30 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Container from 'react-bootstrap/Container'
 
-import {useState, useEffect, useContext} from 'react';
-import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged} from 'firebase/auth';
+import { Row, Col } from 'react-bootstrap'
+import { useState, useEffect, useContext } from 'react';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged } from 'firebase/auth';
 import '../../config/firebase-config';
 
 import { UserContext } from '../../Context/UserContext';
 
-function Register(){
+function Register() {
 
 
-  const {User, setUser} = useContext(UserContext)
+  const { User, setUser } = useContext(UserContext)
   const [token, setToken] = useState('');   // sets token
 
+  const [first_name, setfirst_name] = useState('');
+  const [last_name, setlast_name] = useState('');
+  const [zipcode, setzipcode] = useState('');
+  const [state, setstate] = useState('');
   const [email, setEmail] = useState('');
   const [password, setpassword] = useState('');
 
-  useEffect(()=>{
+  useEffect(() => {
     const auth = getAuth();
     auth.onAuthStateChanged((user) => { // when users login state changes...
-      if(user){
+      if (user) {
         console.log(user)
         // setAuth(true);
         // userCred.getIdToken().then((token)=>{
@@ -30,40 +35,74 @@ function Register(){
     })
   }, [])
 
-  const reg = (e) =>{
+  const reg = (e) => {
     e.preventDefault();
 
     // authenticates with google using api credentals
     const auth = getAuth()
     createUserWithEmailAndPassword(auth, email, password)
-    .then((usercredentials) => {  // If usercredentials are returned, user exist, hence login
-      if(usercredentials){
-        setUser(usercredentials.user)
-      }
-    }).catch((e) => { // else catch and print errors
-      console.log(e.code)
-      console.log(e.message);
-    });
-}
+      .then((usercredentials) => {  // If usercredentials are returned, user exist, hence login
+        if (usercredentials) {
+          setUser(usercredentials.user)
+        }
+      }).catch((e) => { // else catch and print errors
+        console.log(e.code)
+        console.log(e.message);
+      });
+  }
 
-  return(
+  return (
     <>
-      <Container className='p-5'  fluid>
-        <h1>Signup</h1>
-      <Form onSubmit={reg}>
-        <Form.Group className='' controlId='formemail'>
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="email address" onChange={(e) => {setEmail(e.target.value)}}/ >
-        </Form.Group>
-        <Form.Group className='' controlId='formpassword'>
-          <Form.Label>password</Form.Label>
-          <Form.Control type="password" placeholder="password" onChange={(e) => {setpassword(e.target.value)}}/ >
-        </Form.Group>
-        <Button variant="warning" type="submit">
-          Submit
-        </Button>
-      </Form>
-    </Container>
+      <Container className='py-5' fluid>
+        <Row className='justify-content-center'>
+          <Col md={6}>
+            <h1 className='text-center mb-5'>Signup</h1>
+            <Form onSubmit={reg}>
+              <Form.Group className='pt-3' controlId='formemail'>
+                <Form.Label>Email address:</Form.Label>
+                <Form.Control type='email' placeholder='Email Address' onChange={(e) => setEmail(e.target.value)} />
+              </Form.Group>
+              <Form.Group className='pt-3' controlId='formemail'>
+                <Form.Label>Password:</Form.Label>
+                <Form.Control type='password' placeholder='Password' onChange={(e) => setpassword(e.target.value)} />
+              </Form.Group>
+              <Row>
+                <Col md={6}>
+                  <Form.Group className='pt-3' controlId='formfirstname'>
+                    <Form.Label>First Name:</Form.Label>
+                    <Form.Control type='first_name' placeholder='First Name' onChange={(e) => setfirst_name(e.target.value)} />
+                  </Form.Group>
+                </Col>
+                <Col md={6}>
+                  <Form.Group className='pt-3' controlId='formlastname'>
+                    <Form.Label>Last Name:</Form.Label>
+                    <Form.Control type='last_name' placeholder='Last Name' onChange={(e) => setlast_name(e.target.value)} />
+                  </Form.Group>
+                </Col>
+              </Row>
+              <Row className='justify-content-between'>
+                <Col md={6}>
+                  <Form.Group className='pt-3' controlId='formzipcode'>
+                    <Form.Label>Zipcode:</Form.Label>
+                    <Form.Control type='zipcode' placeholder='Zipcode' onChange={(e) => setzipcode(e.target.value)} />
+                  </Form.Group>
+                </Col>
+                <Col md={6}>
+                  <Form.Group className='pt-3' controlId='formstate'>
+                    <Form.Label>State:</Form.Label>
+                    <Form.Control type='state' placeholder='State' onChange={(e) => setstate(e.target.value)} />
+                  </Form.Group>
+                </Col>
+              </Row>
+              <div className='d-flex justify-content-end pt-3'>
+                <Button variant='warning' type='submit' className='ml-auto'>
+                  Submit
+                </Button>
+              </div>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 }
