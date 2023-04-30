718579a6ba3c05b58c1ecd88fb782cba555bb4b7
Steffano Martinez <steffano1213@gmail.com>
2023-04-30 12:48:46 -0400

    Added footer logos clickable, just a nice touch

--- frontend/src/components/footer/Footer.js
+++ frontend/src/components/footer/Footer.js
@@ -10,9 +10,15 @@ export default function MyFooter() {
         <>
             <div className="main-footer"> {/* This div holds a class towards the div's for footer. */}
                 <div className="text-center col-sm">
-                    <Image src={logo_ours} alt="Logo_ours" width={175} height={85} />
-                    <Image className="block" src={logo_yelp} alt="Logo_yelp" width={125} height={85} />
-                    <Image className="block" src={logo_tmdb} alt="Logo_tmdb" width={100} height={85} />
+                    <a href="/" target="_blank" rel="noopener noreferrer">
+                        <Image src={logo_ours} alt="Logo_ours" width={175} height={85} />
+                    </a>
+                    <a href="https://www.yelp.com/" target="_blank" rel="noopener noreferrer">
+                        <Image className="block" src={logo_yelp} alt="Logo_yelp" width={125} height={85} />
+                    </a>
+                    <a href="https://www.themoviedb.org/?language=en-US" target="_blank" rel="noopener noreferrer">
+                        <Image className="block" src={logo_tmdb} alt="Logo_tmdb" width={100} height={85} />
+                    </a>
                 </div>
                 <div className="footer-copyright text-center py-1 text-light">Copyright © 2023 by Fried-Bankman Development Group (F.B.D.G)
                 </div>
