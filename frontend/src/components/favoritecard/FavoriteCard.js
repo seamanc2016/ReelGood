88475ab654c26e3e36a832615630eadd48b8ead2
Steffano Martinez <steffano1213@gmail.com>
2023-04-30 15:30:15 -0400

    Not fixed, keeping favorites

--- frontend/src/components/favoritecard/FavoriteCard.js
+++ frontend/src/components/favoritecard/FavoriteCard.js
@@ -1,29 +1,34 @@
 import emptyImage from '../../images/no-image-available.jpg';
+import { useNavigate } from 'react-router-dom';
 
 function FavoriteCard(props) {
+    //Set states and important variables
+    const navigate = useNavigate()
+
     return (
-        <div className="d-flex flex-column border border-gray h-100 mx-1">
-            {/*For the source we're using, actor images come in 278x185 px sizes.*/}
-            {props.actor.profile_path ? (
-                <img alt="..." src={`https://image.tmdb.org/t/p/w185${props.actor.profile_path}`}
-                 style={{ objectFit: 'cover', height: 'calc(278px * 0.7)', width: 'calc(185px * 0.7)' }} />
+        <div className="d-flex flex-column border border-gray h-100 mx-1 recommendation-link" onClick={() => {
+            console.log(props.history);
+            let historyCopy = [...props.history];
+            historyCopy.push(props.movieID);
+            props.setHistory(historyCopy);
+            props.setMovieID(props.movie.id);
+            window.scroll(0, 0);
+            navigate(`/movies/${props.movie.id}`,{ replace: true })
+        }}>
+            {/*For the source we're using, movie images come in 278x185 px sizes.*/}
+            {props.movie.poster_path ? (
+                <img alt="..." src={`https://image.tmdb.org/t/p/w185${props.movie.poster_path}`}
+                    style={{ objectFit: 'cover', height: 'calc(278px * 0.7)', width: 'calc(185px * 0.7)' }} />
             ) : (
                 <img alt="..." src={emptyImage} style={{ objectFit: 'cover', height: 'calc(278px * 0.7)', width: 'calc(185px * 0.7)' }} />
             )}
-            <div style={{maxWidth: 'calc(185px * 0.7)'}}>
-                <h6 className="text-center text-wrap">{props.actor.name}</h6>
-                <p className="text-center text-wrap m-0"><small>{props.actor.character}</small></p>
+            <div style={{ maxWidth: 'calc(185px * 0.7)' }}
+            >
+                <h6 className="text-center text-wrap">{props.movie.title}</h6>
             </div>
         </div>
     );
 };
 
-export default FavoriteCard;
 
-// ActorCard.defaultProps = {
-//     actor: {
-//         profile_path: '/83o3koL82jt30EJ0rz4Bnzrt2dd.jpg',
-//         name: 'Chris Pratt',
-//         character: 'Mario (voice)'
-//     }
-// }
+export default FavoriteCard;
