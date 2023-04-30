8c1127d8f2ba33c28f9c124cfb6733b7d1c74ae8
seamanc2016 <seamanc2016@fau.edu>
2023-04-30 12:46:13 -0400

    Added favorite button functionality

--- frontend/src/components/favoritebutton/FavoriteButton.js
+++ frontend/src/components/favoritebutton/FavoriteButton.js
@@ -15,25 +15,43 @@ function FavoriteButton(props) {
     //Set states
     let [response, setResponse] = useState(null);
     let [error, setError] = useState(null);
+    let [loading, setLoading] = useState(false);
     let [favorite, setFavorite] = useState(false);
 
     //Set proper state for Favorite button using DB data
-    function getFavorites() {
+    function getFavorites(movieID) {
+        //Clear previous requests
+        setError(null);
+        setResponse(null)
+        setFavorite(false)
+        setLoading(true);
+
         axios.get(`/favorites/${userID}`, { //Modify path as necessary
         })
             .then(function (response) {
                 //On success
                 //Take the response array and try to find the current movie ID in it.
+
+                // setFavorite(!response.data.includes(movieID));
+                setResponse(response.data);
+                setFavorite(response.data.includes(Number(movieID)));
+                setLoading(false);
                 //If it's in there, that movie was favorited. So the initial state needs to be setFavorite(true)
                 //Else, setFavorite(false)
-                console.log(response); //Or just check the response in Network
             })
             .catch(function (error) {
                 // On error
                 //Assume the code doesn't have a stroke for now
+                setError(error.response.data);
+
                 console.log(error);
             });
     }
+
+    //Call function whenever this component re-renders
+    useEffect(() => {
+        getFavorites(movieID);
+    }, [movieID])
 
     //Now after the button's state is initially set, here's how the user can change it dynamically
     //For favoriting the movie
@@ -42,7 +60,7 @@ function FavoriteButton(props) {
         setFavorite(true);
 
         //Make axios call to add favorite endpoint
-        axios.post(`/Favorite`, {
+        axios.post(`/favorite`, {
             movieId: props.movieID,
             uid: userID
         })
@@ -63,7 +81,7 @@ function FavoriteButton(props) {
         setFavorite(false);
 
         //Make axios call to add favorite endpoint
-        axios.put(`/Favorite`, {
+        axios.put(`/favorite`, {
             movieId: props.movieID,
             uid: userID
         })
@@ -79,11 +97,21 @@ function FavoriteButton(props) {
 
 
     return (
+
         <>
-            {favorite ? (<button type="button" className="btn btn-danger me-1" onClick={removeFromFavorites}>Unfavorite</button>)
-                :
-                (<button type="button" className="btn btn-success me-1" onClick={addToFavorites}>Favorite</button>)
-            }
+            {loading && (
+                <button type="button" className="btn btn-dark me-1" disabled>Checking...</button>
+            )}
+
+            {response && (
+                <>
+                    {favorite ? (
+                        <button type="button" className="btn btn-danger me-1" onClick={removeFromFavorites}>Unfavorite</button>
+                    ): (
+                        <button type="button" className="btn btn-success me-1" onClick={addToFavorites}>Favorite</button>
+                    )}
+                </>
+            )}
         </>
     )
 }
